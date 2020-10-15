76f5c299c9eb4cd2cb77dd0783b125d8b41cec2d
TanyaStk <64659217+TanyaStk@users.noreply.github.com>
2020-10-15 20:20:02 +0300

    Develop

--- src/main/java/com/project/Company.java
+++ src/main/java/com/project/Company.java
@@ -1,4 +1,154 @@
 package com.project;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static java.lang.String.format;
+
 public class Company {
+    private String companyName;
+    private String abbreviation;
+    private Date updateDate;
+    private String address;
+    private Date foundationDate;
+    private int numberOfEmployees;
+    private String auditor;
+    private String phoneNumber;
+    private String email;
+    private String industry;
+    private String typeOfBusiness;
+    private String link;
+
+    public Company(String inf) {
+        String[] words = inf.split(";");
+        try {
+            companyName = words[0];
+            abbreviation = words[1];
+            updateDate = new SimpleDateFormat("dd.MM.yyyy").parse(words[2]);
+            address = words[3];
+            foundationDate = new SimpleDateFormat("dd.MM.yyyy").parse(words[4]);
+            numberOfEmployees = Integer.parseInt(words[5]);
+            auditor = words[6];
+            phoneNumber = words[7];
+            email = words[8];
+            industry = words[9];
+            typeOfBusiness = words[10];
+            link = words[11];
+        } catch (Exception ex) {
+            System.out.println("Exception detected: " + ex.getLocalizedMessage());
+        }
+    }
+
+    public String companyInformation() {
+        String inf = "";
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+            inf = ("Company name: " + companyName + "\n" +
+                    "Abbreviation: " + abbreviation + "\n" +
+                    "Industry: " + industry + "\n" +
+                    "Date of foundation: " + format.format(foundationDate) + "\n" +
+                    "Number of employees: " + numberOfEmployees);
+        } catch (Exception ex) {
+            System.out.println("Exception detected: " + ex.getLocalizedMessage());
+        }
+        return inf;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setFoundationDate(Date foundationDate) {
+        this.foundationDate = foundationDate;
+    }
+
+    public void setNumberOfEmployees(int amount) {
+        this.numberOfEmployees = amount;
+    }
+
+    public void setAuditor(String name) {
+        this.auditor = name;
+    }
+
+    public void setPhoneNumber(String number) {
+        this.phoneNumber = number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public void setTypeOfBusiness(String typeOfBusiness) {
+        this.typeOfBusiness = typeOfBusiness;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCompanyName() {
+        return this.companyName;
+    }
+
+    public String getAbbreviation() {
+        return this.abbreviation;
+    }
+
+    public Date getUpdateDate() {
+        return this.updateDate;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Date getFoundationDate() {
+        return this.foundationDate;
+    }
+
+    public int getNumberOfEmployees() {
+        return this.numberOfEmployees;
+    }
+
+    public String getAuditor() {
+        return this.auditor;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getIndustry() {
+        return this.industry;
+    }
+
+    public String getTypeOfBusiness() {
+        return this.typeOfBusiness;
+    }
+
+    public String getLink() {
+        return this.link;
+    }
+
 }
--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,4 +1,74 @@
 package com.project;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Company> companies = readFromFileIntoList();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Insert dates from to (e.g. dd.MM.yyyy)");
+            Date from = new SimpleDateFormat("dd.MM.yyyy").parse(scanner.next());
+            Date to = new SimpleDateFormat("dd.MM.yyyy").parse(scanner.next());
+            List<String> requested = chooseCompaniesByFoundation(from, to, companies);
+            writeStatistics(from, to, requested.size());
+            fileWriting(requested);
+        } catch (Exception ex) {
+            System.out.println("Exception detected: " + ex.getLocalizedMessage());
+        }
+    }
+
+    static List<Company> readFromFileIntoList() throws IOException {
+        Scanner scanner = new Scanner(new File("Companies.csv"));
+        List<Company> companies = new ArrayList<>();
+        while (scanner.hasNext()) {
+            Company company = new Company(scanner.nextLine());
+            companies.add(company);
+        }
+        return companies;
+    }
+
+    static List<String> chooseCompaniesByFoundation(
+            Date from, Date to, List<Company> companies) {
+        List<String> reqComp = new ArrayList<>();
+        for (Company company : companies) {
+            if (company.getFoundationDate().after(from) &&
+                    company.getFoundationDate().before(to)) {
+                reqComp.add(company.companyInformation());
+            }
+        }
+        return reqComp;
+    }
+
+    static void writeStatistics(Date from, Date to, int amountFound) {
+        SimpleDateFormat formatReq = new SimpleDateFormat("dd.MM.yyyy");
+        SimpleDateFormat formatNow = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        try (FileWriter writer =
+                     new FileWriter(new File("logfile.txt"), true)) {
+            Date now = new Date();
+            writer.write("Date and time of running: " + formatNow.format(now) + "\n");
+            writer.write("Inserted dates: " +
+                    formatReq.format(from) + " - " + formatReq.format(to) + "\n");
+            writer.write("Amount of matching notes: " + amountFound + "\n");
+        } catch (IOException ex) {
+            System.out.println("Exception detected: " + ex.getLocalizedMessage());
+        }
+    }
+
+    static void fileWriting(List<String> inf) throws IOException {
+        try (FileWriter writer =
+                     new FileWriter(new File("OutputFile.csv"))) {
+            for (String strings : inf) {
+                writer.append(strings);
+                writer.append(",");
+                writer.append(System.lineSeparator());
+            }
+        }
+    }
 }
